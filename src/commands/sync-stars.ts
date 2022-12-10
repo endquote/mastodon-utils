a68b77e3873be71354f50d2b4ef4289fd0605555
Josh Santangelo <josh@endquote.com>
2022-12-10 14:31:22 -0800

    stars from local feed

--- src/commands/sync-stars.ts
+++ src/commands/sync-stars.ts
@@ -2,7 +2,6 @@ import { Command } from "@oclif/core";
 import axios from "axios";
 import "axios-debug-log/enable";
 import * as dotenv from "dotenv";
-import parseLinkHeader from "parse-link-header";
 import { Config } from "../config";
 import { sharedFlags } from "../constants";
 
@@ -34,56 +33,30 @@ export default class SyncStars extends Command {
       auth: { username: config.feedbinUser!, password: config.feedbinPass! },
     });
 
-    // get mastodon followings
-    const following: any[] = [];
-    let url = `/v1/accounts/${config.accountId}/following`;
-    while (url) {
-      const res = await mastodon.get(url);
-      following.push(...res.data);
-      const links = parseLinkHeader(res.headers.link);
-      url = links && links.next ? links.next.url : "";
-    }
-
     // get starred entries on feedbin
     const starIds: number[] = (await feedbin.get("/starred_entries.json")).data;
-    const starEntries: any[] = [];
+    let stars: any[] = [];
 
     // you can get 100 at a time
     while (starIds.length > 0) {
       const page = starIds.splice(0, 100);
       const params = { ids: page.join(",") };
       const entries = (await feedbin.get("/entries.json", { params })).data;
-      starEntries.push(...entries);
+      stars.push(...entries);
     }
 
-    // match starred entries with mastodon users
-    const stars = starEntries
-      .map((s) => ({
-        entry: s,
-        user: following.find((f) => s.url.includes(f.url)),
-      }))
-      .filter((s) => s.user);
+    // get starred entries from mastodon
+    stars = stars.filter((s) => s.url.includes(config.instance));
 
     for (const star of stars) {
-      // search for the post on my instance
-      const res = await mastodon.get(`/v2/search`, {
-        params: {
-          q: star.entry.url,
-          following: true,
-          type: "statuses",
-          resolve: true,
-          account_id: star.user.acct,
-        },
-      });
-
-      const id = res.data.statuses?.[0]?.id;
+      const id = star.url.split("/").pop();
       if (id) {
         // favourite the post
         await mastodon.post(`/v1/statuses/${id}/favourite`);
 
         // delete the star on feedbin
         await feedbin.delete("/starred_entries.json", {
-          data: { starred_entries: [star.entry.id] },
+          data: { starred_entries: [star.id] },
         });
       }
     }
